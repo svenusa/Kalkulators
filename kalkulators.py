fb5bba5d8d34097e3e8df99536540be758c7472e
svenusa <145644782+svenusa@users.noreply.github.com>
2024-05-16 09:53:31 +0300

    bum bam

--- kalkulators.py
+++ kalkulators.py
@@ -49,25 +49,24 @@ def Vienads():
     return 0
 
 
+Display=Entry(logs,width=25,bd=15,font=("Roman",20),justify="right")#Lodziņš spociņš
 
-Display=Entry(logs,width=15,bd=10,font=("Arial Black",20),justify="right")#Lodziņš spociņš
-
-btn0=Button(logs,text="0",padx="40",pady="20",command=lambda:btnClick(0))
-btn1=Button(logs,text="1",padx="40",pady="20",command=lambda:btnClick(1))
-btn2=Button(logs,text="2",padx="40",pady="20",command=lambda:btnClick(2))
-btn3=Button(logs,text="3",padx="40",pady="20",command=lambda:btnClick(3))
-btn4=Button(logs,text="4",padx="40",pady="20",command=lambda:btnClick(4))
-btn5=Button(logs,text="5",padx="40",pady="20",command=lambda:btnClick(5))
-btn6=Button(logs,text="6",padx="40",pady="20",command=lambda:btnClick(6))
-btn7=Button(logs,text="7",padx="40",pady="20",command=lambda:btnClick(7))
-btn8=Button(logs,text="8",padx="40",pady="20",command=lambda:btnClick(8))
-btn9=Button(logs,text="9",padx="40",pady="20",command=lambda:btnClick(9))
-btnSask=Button(logs,text="+",padx='40',pady="20",command=lambda:btnCommand("+") )
-btnAtn=Button(logs,text="-",padx='40',pady="20",command=lambda:btnCommand("-") )
-btnReiz=Button(logs,text="*",padx='40',pady="20",command=lambda:btnCommand("*") )
-btnDal=Button(logs,text="/",padx='40',pady="20",command=lambda:btnCommand("/") )
-btnC=Button(logs,text="C",padx='40',pady="20",command=notirit )
-btnEQ=Button(logs,text="=",padx='40',pady="20",command=Vienads )
+btn0=Button(logs,text="0",padx="40",pady="20",bg="#F5B7B1",font=("Roman",15),command=lambda:btnClick(0))
+btn1=Button(logs,text="1",padx="40",pady="20",bg="#F5B7B1",font=("Roman",15),command=lambda:btnClick(1))
+btn2=Button(logs,text="2",padx="40",pady="20",bg="#F5B7B1",font=("Roman",15),command=lambda:btnClick(2))
+btn3=Button(logs,text="3",padx="40",pady="20",bg="#F5B7B1",font=("Roman",15),command=lambda:btnClick(3))
+btn4=Button(logs,text="4",padx="40",pady="20",bg="#F5B7B1",font=("Roman",15),command=lambda:btnClick(4))
+btn5=Button(logs,text="5",padx="40",pady="20",bg="#F5B7B1",font=("Roman",15),command=lambda:btnClick(5))
+btn6=Button(logs,text="6",padx="40",pady="20",bg="#F5B7B1",font=("Roman",15),command=lambda:btnClick(6))
+btn7=Button(logs,text="7",padx="40",pady="20",bg="#F5B7B1",font=("Roman",15),command=lambda:btnClick(7))
+btn8=Button(logs,text="8",padx="40",pady="20",bg="#F5B7B1",font=("Roman",15),command=lambda:btnClick(8))
+btn9=Button(logs,text="9",padx="40",pady="20",bg="#F5B7B1",font=("Roman",15),command=lambda:btnClick(9))
+btnSask=Button(logs,text="+",padx='40',pady="20",bg="#EC7063",font=("Roman",15),command=lambda:btnCommand("+") )
+btnAtn=Button(logs,text="-",padx='40',pady="20",bg="#EC7063",font=("Roman",15),command=lambda:btnCommand("-") )
+btnReiz=Button(logs,text="*",padx='40',pady="20",bg="#EC7063",font=("Roman",15),command=lambda:btnCommand("*") )
+btnDal=Button(logs,text="/",padx='40',pady="20",bg="#EC7063",font=("Roman",15),command=lambda:btnCommand("/") )
+btnC=Button(logs,text="C",padx='40',pady="20",bg="#F5B7B1",font=("Roman",15),command=notirit )
+btnEQ=Button(logs,text="=",padx='40',pady="20",bg="#F5B7B1",font=("Roman",15),command=Vienads )
 
 Display.grid(row=0, column=0, columnspan=4) #Atrašanās vieta pogām
 btn7.grid(row=1,column=0)
