9173486b7586dfbf10b5e499fd1d91082fedfc0b
svenusa <145644782+svenusa@users.noreply.github.com>
2024-05-16 08:59:25 +0300

    Update kalkulators.py

--- kalkulators.py
+++ kalkulators.py
@@ -19,32 +19,62 @@ def btnCommand(command):
     Display.delete(0,END)
     return 0
 
+def Vienads():
+    num2=int(Display.get())
+    result=0
+    if mathOp=="+":
+        result=num1+num2
+    elif mathOp=="-":
+        result=num1-num2
+    elif mathOp=="*":
+        result=num1*num2
+    elif mathOp=="/":
+        result=num1/num2
+    else:
+        result=0
+        Display.delete(0,END)
+        Display.insert(0,str(result))
+    return 0
+
+
 Display=Entry(logs,width=15,bd=10,font=("Arial Black",20),justify="right")#Lodziņš spociņš
 
-btn0=Button(logs,text="0",padx="40",pady="20")
-btn1=Button(logs,text="1",padx="40",pady="20")
-btn2=Button(logs,text="2",padx="40",pady="20")
-btn3=Button(logs,text="3",padx="40",pady="20")
-btn4=Button(logs,text="4",padx="40",pady="20")
-btn5=Button(logs,text="5",padx="40",pady="20")
-btn6=Button(logs,text="6",padx="40",pady="20")
-btn7=Button(logs,text="7",padx="40",pady="20")
-btn8=Button(logs,text="8",padx="40",pady="20")
-btn9=Button(logs,text="9",padx="40",pady="20")
+btn0=Button(logs,text="0",padx="40",pady="20",command=lambda:btnClick(0))
+btn1=Button(logs,text="1",padx="40",pady="20",command=lambda:btnClick(1) )
+btn2=Button(logs,text="2",padx="40",pady="20",command=lambda:btnClick(2) )
+btn3=Button(logs,text="3",padx="40",pady="20",command=lambda:btnClick(3) )
+btn4=Button(logs,text="4",padx="40",pady="20",command=lambda:btnClick(4) )
+btn5=Button(logs,text="5",padx="40",pady="20",command=lambda:btnClick(5) )
+btn6=Button(logs,text="6",padx="40",pady="20",command=lambda:btnClick(6) )
+btn7=Button(logs,text="7",padx="40",pady="20",command=lambda:btnClick(7) )
+btn8=Button(logs,text="8",padx="40",pady="20",command=lambda:btnClick(8) )
+btn9=Button(logs,text="9",padx="40",pady="20",command=lambda:btnClick(9) )
+btnSask=Button(logs,text="+",padx='40',pady="20",command=lambda:btnCommand("+") )
+btnAtn=Button(logs,text="-",padx='40',pady="20",command=lambda:btnCommand("-") )
+btnReiz=Button(logs,text="*",padx='40',pady="20",command=lambda:btnCommand("*") )
+btnDal=Button(logs,text="/",padx='40',pady="20",command=lambda:btnCommand("/") )
+btnC=Button(logs,text="C",padx='40',pady="20",command=lambda:btnCommand("C") )
+btnEQ=Button(logs,text="=",padx='40',pady="20",command=lambda:btnCommand("="),command=Vienads )
 
 Display.grid(row=0, column=0, columnspan=4) #Atrašanās vieta pogām
 btn7.grid(row=1,column=0)
 btn8.grid(row=1,column=1)
 btn9.grid(row=1,column=2)
+btnSask.grid(row=1,column=3)
 
 btn4.grid(row=2,column=0)
 btn5.grid(row=2,column=1)
 btn6.grid(row=2,column=2)
+btnAtn.grid(row=2,column=3)
 
 btn1.grid(row=3,column=0)
 btn2.grid(row=3,column=1)
 btn3.grid(row=3,column=2)
+btnReiz.grid(row=3,column=3)
 
 btn0.grid(row=4,column=0)
+btnC.grid(row=4,column=1)
+btnEQ.grid(row=4,column=2)
+btnDal.grid(row=4,column=3)
 
 logs.mainloop()
