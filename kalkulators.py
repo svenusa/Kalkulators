57a5b3f841ef93135e23ba221e3e489d024dd905
svenusa <145644782+svenusa@users.noreply.github.com>
2024-05-16 09:28:57 +0300

    Update kalkulators.py

--- kalkulators.py
+++ kalkulators.py
@@ -3,6 +3,17 @@ from tkinter import*
 logs=Tk()
 logs.title("Kalkulators")
 
+num1 = 0
+mathOp = ""
+
+def notirit():
+    global num1
+    global mathOp
+    Display.delete(0,END)
+    num1=0
+    mathOp=""
+    return 0
+
 def btnClick(number):
     current=Display.get()#Nolasa skaitli
     Display.delete(0,END)#Nodzēšs skaitļus
@@ -20,40 +31,37 @@ def btnCommand(command):
     return 0
 
 def Vienads():
-    num2=int(Display.get())
-    result=0
-    if mathOp=="+":
-        result=num1+num2
-    elif mathOp=="-":
-        result=num1-num2
-    elif mathOp=="*":
-        result=num1*num2
-    elif mathOp=="/":
-        result=num1/num2
-    else:
-        result=0
-        Display.delete(0,END)
-        Display.insert(0,str(result))
+    global num1
+    global mathOp
+    num2 = int(Display.get())
+    result = 0
+    if mathOp == "+":
+        result = num1 + num2
+    elif mathOp == "-":
+        result = num1 - num2
+    elif mathOp == "*":
+        result = num1 * num2
+    elif mathOp == "/":
+        result = num1 / num2
+    
+    Display.delete(0, END)
+    Display.insert(0, str(result))
     return 0
 
-def notirit():
-    Display.delete(0,END)
-    num1=0
-    mathOp=""
-    return 0
+
 
 Display=Entry(logs,width=15,bd=10,font=("Arial Black",20),justify="right")#Lodziņš spociņš
 
 btn0=Button(logs,text="0",padx="40",pady="20",command=lambda:btnClick(0))
-btn1=Button(logs,text="1",padx="40",pady="20",command=lambda:btnClick(1) )
-btn2=Button(logs,text="2",padx="40",pady="20",command=lambda:btnClick(2) )
-btn3=Button(logs,text="3",padx="40",pady="20",command=lambda:btnClick(3) )
-btn4=Button(logs,text="4",padx="40",pady="20",command=lambda:btnClick(4) )
-btn5=Button(logs,text="5",padx="40",pady="20",command=lambda:btnClick(5) )
-btn6=Button(logs,text="6",padx="40",pady="20",command=lambda:btnClick(6) )
-btn7=Button(logs,text="7",padx="40",pady="20",command=lambda:btnClick(7) )
-btn8=Button(logs,text="8",padx="40",pady="20",command=lambda:btnClick(8) )
-btn9=Button(logs,text="9",padx="40",pady="20",command=lambda:btnClick(9) )
+btn1=Button(logs,text="1",padx="40",pady="20",command=lambda:btnClick(1))
+btn2=Button(logs,text="2",padx="40",pady="20",command=lambda:btnClick(2))
+btn3=Button(logs,text="3",padx="40",pady="20",command=lambda:btnClick(3))
+btn4=Button(logs,text="4",padx="40",pady="20",command=lambda:btnClick(4))
+btn5=Button(logs,text="5",padx="40",pady="20",command=lambda:btnClick(5))
+btn6=Button(logs,text="6",padx="40",pady="20",command=lambda:btnClick(6))
+btn7=Button(logs,text="7",padx="40",pady="20",command=lambda:btnClick(7))
+btn8=Button(logs,text="8",padx="40",pady="20",command=lambda:btnClick(8))
+btn9=Button(logs,text="9",padx="40",pady="20",command=lambda:btnClick(9))
 btnSask=Button(logs,text="+",padx='40',pady="20",command=lambda:btnCommand("+") )
 btnAtn=Button(logs,text="-",padx='40',pady="20",command=lambda:btnCommand("-") )
 btnReiz=Button(logs,text="*",padx='40',pady="20",command=lambda:btnCommand("*") )
